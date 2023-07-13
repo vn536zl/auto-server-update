034cd23bc6f43b1d030354e43bbb1017b4bf1978
JISAAPP <john.hayward@jbhunt.com>
2023-07-13 12:36:57 -0500

    commented file and better logging

--- main.py
+++ main.py
@@ -13,11 +13,16 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
 
 webpage_url = "https://www.minecraft.net/en-us/download/server/bedrock"
+# Replace value with location of your server
 server_directory = "old_server"
+# Use IP and Port of your server
 server = BedrockServer.lookup("localhost:19132")
 
 
+# Use selenium to retrieve the download url of new Bedrock Server
 def get_file_link():
+    print('Getting Most recent Bedrock Server Release...')
+
     browser = webdriver.Edge()
     browser.get(webpage_url)
 
@@ -32,7 +37,10 @@ def get_file_link():
             time.sleep(1)
 
 
+# Use Request and zipfile to download and unzip the new version
 def get_file(url):
+    print('Retrieving updated files...')
+
     for i in range(3):
         try:
             response = requests.get(url)
@@ -44,7 +52,10 @@ def get_file(url):
             time.sleep(3)
 
 
+# Copy necessary files to the old servers location
 def copy_files():
+    print('Copying new files...')
+
     file_names = ['bedrock_server.exe', 'bedrock_server.pdb', 'release-notes.txt']
     folder_names = ['behavior_packs', 'definitions', 'resource_packs']
     for file in file_names:
@@ -53,17 +64,20 @@ def copy_files():
         copy_tree(f'updated_server/{folder}', f'{server_directory}/{folder}')
 
 
+# Compare the download url version to the currently running server to determine if an update is needed
 def determine_update(download_url):
+    print('Determining if an update is necessary...')
+
     file_version = re.search(r"(\d{1,2}[.])+zip$", download_url)
     to_version = re.search(r"(\d{1,2}[.])+", file_version[0])[0][:-1]
     try:
         server_status = server.status()
         from_version = server_status.version.name
-        print(from_version)
     except TimeoutError:
         print("Server Status Timeout: Continue update")
         return True
 
+    print(f"Current Server Version: {from_version}")
     from_version_list = from_version.split('.')
     to_version_list = to_version.split('.')
     for i in range(len(from_version_list)):
@@ -75,7 +89,10 @@ def determine_update(download_url):
     return False
 
 
+# Stop the bedrock_server.exe program from running if it is
 def kill_server():
+    print("Stopping old server if it's running...")
+
     processes = os.popen('tasklist').readlines()
     for process in processes:
         process_name_reg = re.match(r"\w+.exe", process)
@@ -85,7 +102,10 @@ def kill_server():
                 os.system("taskkill /f /im bedrock_server.exe")
 
 
+# restart the server with the new version
 def restart_server():
+    print('Starting Updated Server...')
+
     os.chdir(server_directory)
     os.system("bedrock_server")
 
